e9c060e1977b689b2e1ea4f236fe91c77c556b48
StutiiiG <94684107+StutiiiG@users.noreply.github.com>
2022-08-10 01:03:07 +0530

    Create blur&smooth.py

---
+++ blur&smooth.py
@@ -0,0 +1,25 @@
+#Burring & Smoothing
+import cv2
+import numpy as np
+
+cap=cv2.VideoCapture(0)
+
+while(1):
+    ret,frame=cap.read()
+    hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    lower_red=np.array([30,150,50])
+    upper_red=np.array([255,255,180])
+    mask=cv2.inRange(hsv,lower_red,upper_red)
+    res=cv2.bitwise_and(frame,frame,mask=mask)
+
+    kernel=np.ones((15,15),np.float32)/225
+    smoothed=cv2.filter2D(res,-1,kernel)
+    cv2.imshow("Original",frame)
+    cv2.imshow("Smoothed",smoothed)
+
+    k=cv2.waitKey(5) & 0xFF 
+    if k==27:
+        break
+
+cv2.destroyAllWindows()
+cv2.release()
